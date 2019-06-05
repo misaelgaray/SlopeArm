6d6d0e8630e3161177c4e3919c7c62b5d2517870
Misael <misael.garay@tech4trade.com>
2019-06-05 18:31:55 -0500

    Added rotation to arm, not following slope formula yet

---
+++ Assets/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public GameObject joint1;
+	public GameObject joint2;
+	bool keepRotating = false;
+
+	//Por si usas la formula de slope estos parametros
+	//se pueden configurar
+	public float x1 = 1;
+	public float x2 = 2;
+    
+    private string orientation = "left";
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+        	keepRotating = true;
+        	orientation = "right";
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+        	keepRotating = true;
+        	orientation = "left";
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space) || 
+        	Input.GetKeyUp(KeyCode.LeftShift)){
+        	keepRotating = false;
+        }
+
+
+        if(keepRotating){
+        	joint1.transform.Rotate(orientation == "right"?0.5f:-0.5f,0,0,Space.Self);
+        	joint2.transform.Rotate(orientation == "right"?0.5f:-0.5f,0,0,Space.Self);
+        }
+    }
+}
